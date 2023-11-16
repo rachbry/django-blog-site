34f255f33f5982e7838720bf807b224b097b3791
Rachel Cutler <rach_cutler@live.co.uk>
2023-11-16 14:54:26 +0000

    added post_list view

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,9 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)
     template_name = "post_list.html"
+
+# class CommentList(generic.ListView):
+#     queryset = Comment.objects.all()
+#     template_name = "post_list.html"
