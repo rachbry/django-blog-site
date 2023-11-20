a39fd83dbd36c26be365c639a78529e5833dcb13
Rachel Cutler <rach_cutler@live.co.uk>
2023-11-20 15:58:18 +0000

    Added collaborate page

--- about/models.py
+++ about/models.py
@@ -8,4 +8,12 @@ class About(models.Model):
 
     def __str__(self):
         return self.title
-        test
+
+class CollaborateRequest(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Collaboration request from {self.name}"
